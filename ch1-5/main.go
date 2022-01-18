679fe5daa65f9a0ee4fb56f269ea9e8187307f91
glzlaohuai <494763134@qq.com>
2022-01-18 15:30:56 +0800

    in

---
+++ ch1-5/main.go
@@ -0,0 +1,53 @@
+//fetch url using http lib
+package main
+
+
+import(
+
+	"fmt"
+	"os"
+	"net/http"
+	"io"
+)
+
+
+func main(){
+	urls:=os.Args[1:]
+	fmt.Println("fetching url: ",urls)
+
+	fetch(urls)
+}
+
+
+func fetch(urls []string){
+	for _,url:=range urls{
+
+		resp,err:=http.Get(url)
+		if err!=nil {
+			fmt.Fprintf(os.Stderr,"get url failed with error %v",err)
+			os.Exit(1)
+		}else{
+			// fmt.Println("get url succeeded, ",url)
+			// fmt.Println("status: ",resp.Status)
+		// _,err:=ioutil.ReadAll(resp.Body)
+
+		written,err:=	io.Copy(os.Stdout,resp.Body)
+		if err==nil {
+			fmt.Println("written: ",written)
+		}else{
+			fmt.Fprintf(os.Stderr,"copy failed, %v\n",err)
+		}
+
+
+			resp.Body.Close()
+			if err==nil {
+				fmt.Println("content of url :",url)
+				fmt.Println()
+			}else{
+				fmt.Fprintf(os.Stderr,"read content from response failed, error: %v",err)
+			}
+		}
+
+	}
+
+}
