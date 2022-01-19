393f2224aaac5afe5ff717a807919195922bfcea
glzlaohuai <494763134@qq.com>
2022-01-19 12:03:45 +0800

    ...

--- ch3-1/main.go
+++ ch3-1/main.go
@@ -18,4 +18,25 @@ package main
 	 fmt.Printf("binary form: %08b\n",x^y) // the difference
 	//  fmt.Printf("%d, %#[1]o, %#[1]x",x)
 	 fmt.Printf("print with extra signs: %d, %#[1]o, %#[1]x\n",x)
+
+
+	 ascii:='a'
+	 unicode:='你'
+	 newline:='\n'
+
+	 fmt.Printf("%d, %[1]c, %[1]q\n",ascii)
+	 fmt.Printf("%d, %[1]c, %[1]q\n",unicode)
+	 fmt.Printf("%d, %[1]c, %[1]q\n",newline)
+
+	 var float_x float32 = 1<<24
+	 xx:=float_x+1
+	 fmt.Println("x: ",float_x," ++: ",xx)
+	 fmt.Println(xx==float_x)
+
+
+	 if xxx:=float_x+1;xxx==float_x{
+		 fmt.Println("equals")
+	 }
+
+
  }
